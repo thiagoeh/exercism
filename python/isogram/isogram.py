bbc6fe83069098d8d11799b6cebf6a1f22692221
Thiago <thiagoeh@gmail.com>
2018-04-04 13:09:28 -0300

    finished python/isogram

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -1,2 +1,17 @@
 def is_isogram(string):
-    pass
+    chars = []
+    ignored_chars = ['-', ' ']
+
+
+    # Iterate the input string in lowercase form
+    for c in string.lower():
+        # Check if the current char is already in the chars list
+        # AND if it's not one that should be ignored
+        if c in chars and c not in ignored_chars:
+            return(False)
+
+        chars.append(c)
+    
+    # We are at the end of the string, without repeated chars
+    # so it's a isogram!
+    return True
