903a1e0ef2ad8b518efb788a743787c93bef5605
Thiago <thiagoeh@gmail.com>
2018-04-04 12:22:13 -0300

    solved reverse_string.py with a for loop

--- python/reverse-string/reverse_string.py
+++ python/reverse-string/reverse_string.py
@@ -1,2 +1,11 @@
 def reverse(input=''):
-    pass
+    input_len = len(input)
+    reversed  = ''
+
+    for i in range(input_len-1,-1,-1):
+        reversed = reversed + input[i]
+
+    return(reversed)
+
+
+
