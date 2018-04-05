df394789ff529e291a7961214b8ee56eb0666f85
Thiago <thiagoeh@gmail.com>
2018-04-05 10:34:16 -0300

    finished python/rna_transcription

--- python/rna-transcription/rna_transcription.py
+++ python/rna-transcription/rna_transcription.py
@@ -1,2 +1,17 @@
+#import string
+
+
+# Useful docs:
+# https://docs.python.org/3/library/stdtypes.html#str.translate
+# https://docs.python.org/3/library/stdtypes.html#str.maketrans
+
+
+dna_values = 'GCTA'
+rna_values = 'CGAU'
+dna_to_rna_translation = str.maketrans(dna_values,rna_values)
+
 def to_rna(dna_strand):
-    pass
+    return dna_strand.translate(dna_to_rna_translation)
+
+
+
