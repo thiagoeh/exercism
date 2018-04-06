457a40bcfa4fc87e4180d1032ee76eb5d562cddb
Thiago <thiagoeh@gmail.com>
2018-04-06 16:41:52 -0300

    refactored java/twofer to have the same formatted message just once

--- java/two-fer/src/main/java/Twofer.java
+++ java/two-fer/src/main/java/Twofer.java
@@ -1,9 +1,9 @@
 class Twofer {
     String twofer(String name) {
         if(name == null) {
-            return "One for you, one for me."; }
-        else {
-            return "One for " + name + ", one for me."; }
+            name = "you"; }
+
+        return "One for " + name + ", one for me."; 
 
     }
 }
