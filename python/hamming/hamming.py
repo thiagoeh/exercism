4051db5cafaba6065ee612e3dd156a53f0f6367d
Thiago <thiago>
2018-04-07 16:16:09 -0300

    python/hamming some linting

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -4,13 +4,15 @@ def distance(strand_a, strand_b):
         raise ValueError("Strands don't have the same length")
 
     # Iterate every char in strand_a comparing with strand_b
-    # Return False if a difference is found
-    distanceCount = 0
+    # Increment distance_count for every difference found
+    distance_count = 0
     for i in range(len(strand_a)):
         if strand_a[i] != strand_b[i]:
-            distanceCount += 1
+            distance_count += 1
 
-    return distanceCount
+    return distance_count
 
+
+# Basic test calling the script directly
 if __name__ == '__main__':
-    print(distance('ABDA','ABDA'))
+    print(distance('ABDA', 'ABDA'))
