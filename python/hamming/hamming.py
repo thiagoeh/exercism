dec596bd4e52bdddf6a9672c3d36ad3e6dadea18
Thiago <thiago>
2018-04-07 16:00:16 -0300

    python/hamming initial solution

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,2 +1,16 @@
 def distance(strand_a, strand_b):
-    pass
+    # Check if strands have same length
+    if len(strand_a) != len(strand_b):
+        raise ValueError("Strands don't have the same length")
+
+    # Iterate every char in strand_a comparing with strand_b
+    # Return False if a difference is found
+    distanceCount = 0
+    for i in range(len(strand_a)):
+        if strand_a[i] != strand_b[i]:
+            distanceCount += 1
+
+    return distanceCount
+
+if __name__ == '__main__':
+    print(distance('ABDA','ABDA'))
