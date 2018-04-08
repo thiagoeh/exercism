8b25b291640999fd12b55c8cdf4764bd4ada4230
Thiago <thiagoeh@gmail.com>
2018-04-08 17:37:41 -0300

    python/word-count WIP

--- python/word-count/word_count.py
+++ python/word-count/word_count.py
@@ -1,2 +1,16 @@
 def word_count(phrase):
-    pass
+    words = {}
+
+    phrase_stripped = (
+       phrase.lower().replace(':', ' ').replace('.', ' ').replace(',', ' '))
+
+    for word in phrase_stripped.split(' '):
+        word_stripped = word.strip()
+        if word_stripped != '':
+            words[word_stripped] = words.get(word_stripped, 0) + 1
+
+    return words
+
+
+if __name__ == '__main__':
+    print(word_count('bblah bleh bleh blih'))
