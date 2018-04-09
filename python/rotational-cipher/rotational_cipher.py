aad077f27c4383d41e385d91f4a4b91456a17fd4
Thiago <thiagoeh@gmail.com>
2018-04-09 17:15:45 -0300

    python/rotational-cipher first solution

--- python/rotational-cipher/rotational_cipher.py
+++ python/rotational-cipher/rotational_cipher.py
@@ -1,2 +1,12 @@
+import string
+
+cipher_origin = string.ascii_lowercase + string.ascii_uppercase
+
+
 def rotate(text, key):
-    pass
+    cipher_destin = (string.ascii_lowercase[key:] + string.ascii_lowercase[0:key]
+                     + string.ascii_uppercase[key:] + string.ascii_uppercase[0:key])
+
+    cipher_transl = str.maketrans(cipher_origin, cipher_destin)
+
+    return text.translate(cipher_transl)
