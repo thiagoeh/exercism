42ca70d6d80b566b8891d6d99e558807779ef677
Thiago <thiagoeh@gmail.com>
2018-04-09 12:30:23 -0300

    python/bob finished

--- python/bob/bob.py
+++ python/bob/bob.py
@@ -1,2 +1,14 @@
 def hey(phrase):
-    pass
+    phrase = phrase.strip()
+
+    if phrase == '':
+        return 'Fine. Be that way!'
+    elif phrase[-1] == '?':
+        if phrase.isupper():
+            return "Calm down, I know what I'm doing!"
+        else:
+            return 'Sure.'
+    elif phrase.isupper():
+        return 'Whoa, chill out!'
+    else:
+        return 'Whatever.'
