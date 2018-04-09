84216a2638a94c48181cd8e3db4fcdbe89ed07d7
Thiago <thiagoeh@gmail.com>
2018-04-09 16:52:25 -0300

    python/armstrong_numbers first solution

--- python/armstrong-numbers/armstrong_numbers.py
+++ python/armstrong-numbers/armstrong_numbers.py
@@ -1,2 +1,8 @@
 def is_armstrong(number):
-    pass
+    number_len = len(str(number))
+    armstrong_sum = 0
+
+    for n in str(number):
+        armstrong_sum += int(n) ** number_len
+
+    return number == armstrong_sum
