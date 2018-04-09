16adbdcbebfda1e9dde8c894c1b3d7d6ec5580f2
Thiago <thiagoeh@gmail.com>
2018-04-09 11:10:21 -0300

    python/word_count initial working version. it's ugly! needs refactoring!

--- python/word-count/word_count.py
+++ python/word-count/word_count.py
@@ -2,10 +2,16 @@ def word_count(phrase):
     words = {}
 
     phrase_stripped = (
-       phrase.lower().replace(':', ' ').replace('.', ' ').replace(',', ' '))
+           phrase.lower().replace(':', ' ').replace('.', ' ').replace(',', ' ').replace('_', ' ').replace('\t', ' ')
+       )
+
+    phrase_stripped = ''.join( c for c in phrase_stripped if  c not in
+            '?:!/;$#@&%^*()' )
 
     for word in phrase_stripped.split(' '):
         word_stripped = word.strip()
+        word_stripped = word_stripped.strip("\"")
+        word_stripped = word_stripped.strip("'")
         if word_stripped != '':
             words[word_stripped] = words.get(word_stripped, 0) + 1
 
@@ -13,4 +19,4 @@ def word_count(phrase):
 
 
 if __name__ == '__main__':
-    print(word_count('bblah bleh bleh blih'))
+    print(word_count("Joe can't tell between 'large' and large."))
