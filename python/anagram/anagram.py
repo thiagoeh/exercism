0d7f1624f67a156c3a49fa548bde9857e59aa8d1
Thiago <thiagoeh@gmail.com>
2018-04-11 19:06:13 -0300

    python/anagram first solution. not so much pythonic

--- python/anagram/anagram.py
+++ python/anagram/anagram.py
@@ -1,2 +1,6 @@
 def detect_anagrams(word, candidates):
-    pass
+    anagrams = []
+    for candidate in candidates:
+        if sorted(word.lower()) == sorted(candidate.lower()) and word.lower() != candidate.lower():
+            anagrams.append(candidate)
+    return anagrams
