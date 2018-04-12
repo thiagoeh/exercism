26d1649d6de3b57e98f91b29ab22b115f58d0e1b
Thiago <thiagoeh@gmail.com>
2018-04-12 13:24:25 -0300

    csharp/reverse-string WIP

--- csharp/reverse-string/ReverseString.cs
+++ csharp/reverse-string/ReverseString.cs
@@ -4,6 +4,8 @@ public static class ReverseString
 {
     public static string Reverse(string input)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        char[] charArray = input.ToCharArray();
+        Array.Reverse(charArray);
+        return new string(charArray);
     }
 }
