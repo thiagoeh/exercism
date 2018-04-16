5283f3273fa3878ca6bf8a666f9b86504ed390ba
Thiago <thiagoeh@gmail.com>
2018-04-16 18:13:10 -0300

    python/sieve needs refatoring

--- python/sieve/sieve.py
+++ python/sieve/sieve.py
@@ -1,2 +1,9 @@
 def sieve(limit):
-    pass
+    non_primes = set(range(2, limit+1))
+    primes = set()
+    while len(non_primes) > 0:
+        prime = min(non_primes)
+        primes.add(prime)
+        non_primes = non_primes - set(range(prime, limit+1, prime))
+
+    return sorted(primes)
