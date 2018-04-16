210cc974b7ad52c6922154822f3ff9b589331163
Thiago <thiagoeh@gmail.com>
2018-04-16 16:38:10 -0300

    python/series first solution

--- python/series/series.py
+++ python/series/series.py
@@ -1,2 +1,11 @@
 def slices(series, length):
-    pass
+    if length == 0:
+        raise ValueError('length parameter must be greater than 0')
+    if length > len(series):
+        raise ValueError('length parameter is greater than series length')
+
+    series_slices = []
+    for i in range(len(series) - length + 1):
+        series_slices.append(list(map(int, series[i:length + i])))
+
+    return series_slices
