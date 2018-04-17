e68f8272f15a273daad24184f0847c7d8bb6c7b3
Thiago <thiago>
2018-04-17 11:26:42 -0300

    python/atbash-cipher first solution

--- python/atbash-cipher/atbash_cipher.py
+++ python/atbash-cipher/atbash_cipher.py
@@ -1,6 +1,19 @@
+from string import ascii_lowercase
+
+cipher = 'zyxwvutsrqponmlkjihgfedcba'
+strip_size = 5
+
+encode_translation = str.maketrans(ascii_lowercase, cipher)
+decode_translation = str.maketrans(cipher, ascii_lowercase)
+
+
 def encode(plain_text):
-    pass
+    return add_spaces(plain_text.lower().replace('.', '').replace(',', '').replace(' ', '').translate(encode_translation))
 
 
 def decode(ciphered_text):
-    pass
+    return ciphered_text.lower().replace(' ', '').translate(decode_translation)
+
+
+def add_spaces(ciphered_text):
+    return ' '.join([ciphered_text[i:i+strip_size] for i in range(0, len(ciphered_text), strip_size)])
