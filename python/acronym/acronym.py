010128326a1c3aeb3166752ff4bc7c703744e96b
Thiago <thiagoeh@gmail.com>
2018-04-18 12:27:49 -0300

    python/acronym

--- python/acronym/acronym.py
+++ python/acronym/acronym.py
@@ -1,2 +1,8 @@
+import re
+
 def abbreviate(words):
-    pass
+
+    # Split words using any puctuation as separator
+    word_list = re.split('\W+', words)
+    
+    return ''.join([word[0].upper() for word in word_list])
