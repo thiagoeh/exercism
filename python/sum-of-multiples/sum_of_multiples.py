88ca9f641a194ed39cb675ec5c38b7c93f6e42c6
Thiago <thiagoeh@gmail.com>
2018-04-18 12:11:48 -0300

    python/sum-of-multiples first solution

--- python/sum-of-multiples/sum_of_multiples.py
+++ python/sum-of-multiples/sum_of_multiples.py
@@ -1,2 +1,10 @@
 def sum_of_multiples(limit, multiples):
-    pass
+
+    unique_multiples = set()
+
+    # Maybe there is a more 'pythonic' way to do this, without the for loop
+    for multiple in multiples:
+        unique_multiples = unique_multiples.union(
+            set(range(multiple, limit, multiple)))
+
+    return sum(unique_multiples)
