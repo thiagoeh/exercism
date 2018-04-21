8221d48490b478f5397042d7a881e4d82c7b3b2b
Thiago <thiago>
2018-04-21 15:12:50 -0300

    python/kindergarten-garden first solution, before refactoring

--- python/kindergarten-garden/kindergarten_garden.py
+++ python/kindergarten-garden/kindergarten_garden.py
@@ -1,3 +1,35 @@
 class Garden(object):
-    def __init__(self, diagram, students):
-        pass
+    default_students = (
+        'Alice',
+        'Bob',
+        'Charlie',
+        'David',
+        'Eve',
+        'Fred',
+        'Ginny',
+        'Harriet',
+        'Ileana',
+        'Joseph',
+        'Kincaid',
+        'Larry',
+    )
+
+    def __init__(self, diagram, students=default_students):
+        self.diagram = diagram
+        self.students = sorted(students)
+
+        self.plants_names = {
+            'C': 'Clover',
+            'G': 'Grass',
+            'R': 'Radishes',
+            'V': 'Violets',
+        }
+
+    def plants(self, student):
+        student_index = self.students.index(student)
+        rows = self.diagram.split()
+        student_plants = []
+        for row in rows:
+            student_plants.extend(row[student_index*2:student_index*2+2])
+
+        return [self.plants_names[char] for char in student_plants]
