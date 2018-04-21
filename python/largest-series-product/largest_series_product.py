1c8dd54e7e2a000c9ecfc78c50f829465a9b66b3
Thiago <thiago>
2018-04-21 13:26:32 -0300

    python/largest-series-product WIP refactoring

--- python/largest-series-product/largest_series_product.py
+++ python/largest-series-product/largest_series_product.py
@@ -1,3 +1,10 @@
+def serie_product(serie):
+    product = 1
+    for char in serie:
+       product *= int(char) 
+    return product
+
+
 def largest_product(series, size):
     if size < 0:
         raise ValueError('size must be a non-negative integer')
@@ -7,13 +14,10 @@ def largest_product(series, size):
 
     products = set()
     for combination in combinations:
-        multiple = 1
-        for char in combination:
-            multiple *= int(char)
-        products.add(multiple)
+        products.add(serie_product(combination))
 
     # Return the largest product
     return max(products)
 
 
-print(largest_product('23453456353645',3))
+print(largest_product('23453456353645', 3))
