7d9073939f48a5169cc42153b8e179f3b78c4dc1
Thiago <thiago>
2018-04-21 13:19:32 -0300

    python/largest-series-product WIP refactoring

--- python/largest-series-product/largest_series_product.py
+++ python/largest-series-product/largest_series_product.py
@@ -2,8 +2,8 @@ def largest_product(series, size):
     if size < 0:
         raise ValueError('size must be a non-negative integer')
 
-    # Permute all combinations based on the size
-    combinations = [series[i:size+i] for i in range(0, len(series)-size+1)]
+    # Generate all combinations based on the size
+    combinations = (series[i:size+i] for i in range(0, len(series)-size+1))
 
     products = set()
     for combination in combinations:
@@ -14,3 +14,6 @@ def largest_product(series, size):
 
     # Return the largest product
     return max(products)
+
+
+print(largest_product('23453456353645',3))
