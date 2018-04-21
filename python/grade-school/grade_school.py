111053c92e6e5dd84386e2844118d4b9ff341d2e
Thiago <thiago>
2018-04-21 17:39:09 -0300

    python/grade-school fixed typo

--- python/grade-school/grade_school.py
+++ python/grade-school/grade_school.py
@@ -4,24 +4,24 @@ from collections import OrderedDict
 class School(object):
     def __init__(self, name):
         self.name = name
-        self.rooster = OrderedDict()
+        self.roster = OrderedDict()
 
     def add(self, name, grade):
-        self.rooster[name] = grade
+        self.roster[name] = grade
 
     def grade(self, grade_number):
-        return [student for student in self.rooster if self.rooster[student] == grade_number]
+        return [student for student in self.roster if self.roster[student] == grade_number]
 
     def sort(self):
-        # sort the rooster by student name
-        self.rooster = OrderedDict(sorted(self.rooster.items()))
+        # sort the roster by student name
+        self.roster = OrderedDict(sorted(self.roster.items()))
         # A set containing grades from current students
-        grades = set(self.rooster.values())
+        grades = set(self.roster.values())
 
-        rooster_sorted = []
-        # Add to rooster_sorted a tuple for each grade, containing another tuple with students names as the second item in the parent tuple
+        roster_sorted = []
+        # Add to roster_sorted a tuple for each grade, containing another tuple with students names as the second item in the parent tuple
         for grade_ in grades:
             students = [student[0]
-                        for student in self.rooster.items() if student[1] == grade_]
-            rooster_sorted.append((grade_, tuple(students),))
-        return rooster_sorted
+                        for student in self.roster.items() if student[1] == grade_]
+            roster_sorted.append((grade_, tuple(students),))
+        return roster_sorted
