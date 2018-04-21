c5ee793c0b3339ab97e9dcdde67117baa961f0c4
Thiago <thiago>
2018-04-21 18:13:46 -0300

    python/flatten-array first solution

--- python/flatten-array/flatten_array.py
+++ python/flatten-array/flatten_array.py
@@ -1,2 +1,13 @@
+import collections
+
+
 def flatten(iterable):
-    pass
+    flattened = []
+    for item in iterable:
+        if item != None:
+            # Flattens any iterable except strings
+            if isinstance(item, collections.Iterable) and not isinstance(item, str):
+                flattened.extend(flatten(item))
+            else:
+                flattened.append(item)
+    return flattened
