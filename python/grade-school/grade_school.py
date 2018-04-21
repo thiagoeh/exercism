3313d0e17ded2cea9674fcda151010e011b81005
Thiago <thiago>
2018-04-21 17:23:35 -0300

    python/grade-school first solution before refactoring

--- python/grade-school/grade_school.py
+++ python/grade-school/grade_school.py
@@ -1,3 +1,26 @@
+from collections import OrderedDict
+
+
 class School(object):
     def __init__(self, name):
-        pass
+        self.name = name
+        self.rooster = OrderedDict()
+
+    def add(self, name, grade):
+        self.rooster[name] = grade
+
+    def grade(self, grade_number):
+        return [student for student in self.rooster if self.rooster[student] == grade_number]
+
+    def sort(self):
+        self.rooster = OrderedDict(sorted(self.rooster.items()))
+        grades = set(self.rooster.values())
+
+        rooster_sorted = []
+        for grade_ in grades:
+            students_in_grade = []
+            for student in self.rooster.items():
+                if student[1] == grade_:
+                    students_in_grade.append(student[0])
+            rooster_sorted.append((grade_, tuple(students_in_grade),))
+        return rooster_sorted
