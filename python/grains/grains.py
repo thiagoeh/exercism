99b07c49c981a38096441abb2a38251b622beb9c
Thiago <thiagoeh@gmail.com>
2018-05-03 12:36:39 -0300

    python/grains first solution

--- python/grains/grains.py
+++ python/grains/grains.py
@@ -1,6 +1,15 @@
 def on_square(integer_number):
-    pass
+    if not 0 < integer_number <= 64:
+        raise ValueError(
+            'square parameter must be a positive integer up to 64')
+
+    return 2 ** (integer_number - 1)
 
 
 def total_after(integer_number):
-    pass
+    if not 0 < integer_number <= 64:
+        raise ValueError(
+            'square parameter must be a positive integer up to 64')
+
+    # Can be optimized inserting the on_square formula directly, but it would lose readability
+    return sum(on_square(x) for x in range(1, integer_number+1))
